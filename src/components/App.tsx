6f20e642327ee301eb4bb1185d7a51ce78dde62b
Shnurkova <Annshnur@gmail.com>
2024-06-27 16:55:08 +0300

    add

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ImageGallery from "./ImageGallery/ImageGallery";
+import { getImages } from "./img-api";
+import SearchBar from "./SearchBar/SearchBar";
+import Loader from "./Loader/Loader";
+import ErrorMessage from "./ErrorMessage/ErrorMessage";
+import LoadMoreBtn from "./LoadMoreBtn/LoadMoreBtn";
+import ImageModal from "./ImageModal/ImageModal";
+import { Image } from "./App.types";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Responce {
+  results:[];
+  total: number;
 }
 
-export default App
+export default function App() {
+  const [images, setImages] = useState < Image[]>([]);
+  const [error, setError] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(1);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [totalPage, setTotalPage] = useState<boolean>(false);
+ const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
+ const [selectedImageUrl, setSelectedImageUrl] = useState<string>("");
+
+  
+  useEffect(() => {
+    if (searchQuery.trim() === "") {
+      return;
+    }
+  
+  
+    async function fetchImages(): Promise<void> {
+      try {
+        setLoading(true);
+        setError(false);
+
+        // const resonse: Responce = await getImages(searchQuery, page);
+        const resonse = await getImages<Responce>(searchQuery, page);
+        const { results, total } = resonse;
+
+        setImages((prevState) => [...prevState, ...results]);
+        setTotalPage(page < Math.ceil(total / 15));
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchImages();
+  }, [searchQuery, page]);
+
+  const handleSearch= (query: string) =>  {
+    setSearchQuery(query);
+    setPage(1);
+    setImages([]);
+  };
+
+  const hendleLoadMore =  () => {
+    setPage(page + 1);
+  };
+// modal
+    const openModal= (imageUrl: string)  => {
+      setSelectedImageUrl(imageUrl);
+      setModalIsOpen(true);
+    };
+
+  const closeModal: () => void = () => {
+    setSelectedImageUrl("");
+    setModalIsOpen(false);
+  };
+
+
+  return (
+    <div>
+      <SearchBar onSearch={handleSearch} />
+
+      {error && <ErrorMessage />}
+
+      {images.length > 0 && (
+        <ImageGallery items={images} onImageClick={openModal} />
+      )}
+      {totalPage && <LoadMoreBtn onClick={hendleLoadMore} />}
+
+      {loading && <Loader />}
+
+   
+      <ImageModal
+        isOpen={modalIsOpen}
+        onClose={closeModal}
+        imageUrl={selectedImageUrl}
+      />
+    </div>
+  );
+}
